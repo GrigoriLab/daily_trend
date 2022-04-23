d37810b7bb6101a45a76e301c9857a1724d8ff4b
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2022-04-23 22:12:03 +0400

    Improve api

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 from apps.api.v1.trend import TrendView
 
 apipatterns = [
-    path("trends/", TrendView.as_view(), name='trends'),
+    path("search_interest/", TrendView.as_view(), name='trends'),
     path('auth/', obtain_auth_token, name='auth'),
 ]
 
--- apps/api/v1/trend.py
+++ apps/api/v1/trend.py
@@ -1,11 +1,12 @@
 import logging
 
+import pandas as pd
+from django_pandas.io import read_frame
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from apps.crawler.models import Trend
-from django_pandas.io import read_frame
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +15,15 @@ class TrendView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        data = Trend.objects.all().order_by('-value')
+        keyword = request.query_params.get("keyword")
+        data = Trend.objects.all()
+        if keyword:
+            data = data.filter(variable=keyword)
+        data = data.order_by('-value')
         df = read_frame(data, fieldnames=['date', 'variable', 'value'])
-        return Response(df)
+        if keyword:
+            df = df.drop('variable', axis=1)
+            df['date'] = pd.to_datetime(df['date']).astype(str)
+            df = df.set_index('date').astype(str)
+            df = df.to_dict()['value']
+        return Response(data=df)
