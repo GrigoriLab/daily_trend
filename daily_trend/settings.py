896c6135214a3e6c23990dc0dc10a3f62b9187c8
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2022-04-23 17:40:18 +0400

    Move apps into apps folder

--- daily_trend/settings.py
+++ daily_trend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crawler',
-    'api',
+    'apps.crawler',
+    'apps.api',
 ]
 
 MIDDLEWARE = [
